36851a6ba0b719cdfe27c092591ad59bf098a96b
rm721 <rm721@kent.ac.uk>
2022-10-01 19:19:08 +0100

    BES-3: vue setup

---
+++ book-recommendations/src/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  mounted() {
+    fetch("/api/test")
+        .then((response) => response.text())
+        .then((data) => {
+          this.msg = data;
+        });
+  }
+}
+</script>
--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/TestController.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/TestController.java
@@ -1,13 +1,14 @@
 package com.bestreads.bookrecommendations;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class TestController {
-
     @GetMapping("/test")
     public String getTestPage() {
-        return "Hello";
+        return "Hello world";
     }
 }
