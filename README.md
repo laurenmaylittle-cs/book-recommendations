c44b038259ffc07e5a26630fd1331c5e36b504df
rm721 <rm721@kent.ac.uk>
2022-10-07 11:52:23 +0100

    BES-3: added vuetify and updated the README.md

--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 ## running vue
 When you pull the project use
 ```
-npm install
+npm run serve
 ```
 from the [frontend directory](book-recommendations/src/frontend)
 
+## installing mdi icons
+```
+npm install @mdi/font
+```
+from the [frontend directory](book-recommendations/src/frontend)
--- book-recommendations/src/frontend/src/main.js
+++ book-recommendations/src/frontend/src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
 
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify : new Vuetify(),
   render: h => h(App)
 }).$mount('#app')
