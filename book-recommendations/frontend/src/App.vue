0df645a94de06891bdbaafbba60d92a6095cb22f
Lauren Little <lml22@kent.ac.uk>
2022-10-13 14:00:58 +0100

    added vuetify to project

--- book-recommendations/frontend/src/App.vue
+++ book-recommendations/frontend/src/App.vue
@@ -1,29 +1,20 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <nav-bar/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
+import NavBar from '@/components/NavBar'
+export default {
+  name: 'App',
+  components: { NavBar },
+  data: () => ({
+    //
+  })
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-</style>
+</script>
