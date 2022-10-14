485bb4de48a8a238f51a7f6ed2c91f9a2f74c97c
Lauren Little <lml22@kent.ac.uk>
2022-10-14 17:18:23 +0100

    removed logos and unused components

--- book-recommendations/frontend/tests/unit/example.spec.js
+++ book-recommendations/frontend/tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeView from '@/views/HomeView.vue'
 
-describe('HelloWorld.vue', () => {
+describe('HomeView.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(HomeView, {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
