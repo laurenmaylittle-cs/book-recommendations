d7240886dbfa1daf7cded7841ba9caa0f4035425
Lauren Little <lml22@kent.ac.uk>
2022-10-14 17:11:15 +0100

    added security config for our endpoints linked frontend and backend together

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/examplecode/TestController.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/examplecode/TestController.java
@@ -1,9 +1,12 @@
-package com.bestreads.bookrecommendations;
+package com.bestreads.bookrecommendations.examplecode;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class TestController {
 
     @GetMapping("/test")
--- book-recommendations/src/test/java/com/bestreads/bookrecommendations/TestControllerTest.java
+++ book-recommendations/src/test/java/com/bestreads/bookrecommendations/TestControllerTest.java
@@ -1,16 +1,12 @@
 package com.bestreads.bookrecommendations;
 
+import com.bestreads.bookrecommendations.examplecode.TestController;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
