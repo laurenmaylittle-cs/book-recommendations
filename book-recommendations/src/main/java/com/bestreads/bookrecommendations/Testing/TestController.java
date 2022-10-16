d4b6e743eb1df7158fdbe9b7494cb5f9690f074d
MarvanT <marvan.t98@gmail.com>
2022-10-16 09:53:45 +0100

    BES:4: code style fix

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/Testing/TestController.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/Testing/TestController.java
@@ -1,11 +1,10 @@
 package com.bestreads.bookrecommendations.Testing;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Streamable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class TestController {
