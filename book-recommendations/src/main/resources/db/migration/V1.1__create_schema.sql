d8572ae867bb115bdf75ea77a0bb396f99018ae9
Fernando Ramirez De Aguilar C <fr223@kent.ac.uk>
2022-10-19 19:12:34 +0100

    drop exixting test_book tables and create a new one in the existing schema

--- book-recommendations/src/main/resources/db/migration/V1.1__create_schema.sql
+++ book-recommendations/src/main/resources/db/migration/V1.1__create_schema.sql
@@ -1,1 +1,10 @@
-CREATE SCHEMA IF NOT EXISTS best_reads
+CREATE SCHEMA IF NOT EXISTS best_reads;
+
+DROP TABLE IF EXISTS test_book;
+
+DROP TABLE IF EXISTS best_reads.test_book;
+
+CREATE TABLE best_reads.test_book (
+  id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title text
+);
