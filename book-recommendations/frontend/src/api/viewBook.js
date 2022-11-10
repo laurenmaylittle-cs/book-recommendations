3b79e3bb7f90a87f4c1b13909407ece7fe3188c3
rm721 <rm721@kent.ac.uk>
2022-11-10 10:26:05 +0000

    BES-51: updated endpoint to point towards '/book'

--- book-recommendations/frontend/src/api/viewBook.js
+++ book-recommendations/frontend/src/api/viewBook.js
@@ -1,4 +1,4 @@
 export async function getBookInfo (id) {
-  return await fetch('http://localhost:8080/api/view-book?id='+ id).then((response) => response.json())
+  return await fetch('http://localhost:8080/api/book?id='+ id).then((response) => response.json())
 }
 
