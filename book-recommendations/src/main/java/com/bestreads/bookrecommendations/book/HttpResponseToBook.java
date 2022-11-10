f359525f1055b4b979dc72cf7398fbfd3134249f
rm721 <rm721@kent.ac.uk>
2022-11-10 11:10:04 +0000

    BES-51: added lines between definitions

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/book/HttpResponseToBook.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/book/HttpResponseToBook.java
@@ -3,10 +3,12 @@ package com.bestreads.bookrecommendations.book;
 import com.bestreads.bookrecommendations.googlebooks.Item;
 
 import java.net.http.HttpResponse;
+
 import java.util.List;
 
 public interface HttpResponseToBook {
 
     List<Book> extractFromHttpResponse(HttpResponse<String> httpResponse);
+
     Item extractBookFromHttpResponse(HttpResponse<String> httpResponse);
 }
