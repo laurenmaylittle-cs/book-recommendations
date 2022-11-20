3ae77941272bf37e0b31b3f4154727b2ade74b00
MarvanT <marvan.t98@gmail.com>
2022-11-20 19:01:45 +0000

    BES-69: Tests added

---
+++ book-recommendations/src/test/java/com/bestreads/bookrecommendations/Testing/TestingSecurityControllerTest.java
@@ -0,0 +1,66 @@
+package com.bestreads.bookrecommendations.Testing;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.bestreads.bookrecommendations.security.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(TestingSecurityController.class)
+@Import(SecurityConfig.class)
+class TestingSecurityControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private TestBookRepository testBookRepository;
+
+  @Test
+  void returnPublic_openToPublic() throws Exception {
+    mockMvc.perform(get("/api/public/get-public"))
+        .andExpect(content().string("This is open to public"));
+  }
+
+  @Test
+  void returnPrivate_whenUnauthenticatedThen401() throws Exception {
+    mockMvc.perform(get("/api/private/get-private"))
+        .andExpect(status().isUnauthorized());
+  }
+
+  @Test
+  @WithMockUser
+  void returnPrivate_whenAuthenticatedThenReturnsString() throws Exception {
+    mockMvc.perform(get("/api/private/get-private"))
+        .andExpect(status().isOk())
+        .andExpect(content().string("This is only for private stuff"));
+  }
+
+  @Test
+  void privateScoped_whenUnauthenticatedThen401() throws Exception {
+    mockMvc.perform(get("/api/private/private-scoped"))
+        .andExpect(status().isUnauthorized());
+  }
+
+  @Test
+  @WithMockUser
+  void privateScoped_whenNoPrivilegesThen403() throws Exception {
+    mockMvc.perform(get("/api/private-scoped"))
+        .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithMockUser(authorities = "SCOPE_read:messages")
+  void privateScoped_withCorrectPrivilegesReturnsString() throws Exception {
+    mockMvc.perform(get("/api/private-scoped"))
+        .andExpect(status().isOk())
+        .andExpect(content().string("Scoped private"));
+  }
+}
