73fe7ef4dfe0b04b978167a640a57d866e0b9205
rm721 <rm721@kent.ac.uk>
2022-11-21 10:20:35 +0000

    BES-51: removed default values and updated 'v-if' statements

--- book-recommendations/frontend/src/components/ViewBookDetails.vue
+++ book-recommendations/frontend/src/components/ViewBookDetails.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="ma-12 pa-2">
     <h1
-      v-if="title !== 'defaultTitle'"
+      v-if="title"
     >
       <v-icon>mdi-book-open-page-variant</v-icon>
       {{ title }}
     </h1>
     <!-- TODO BES-70 ensure this works with multiple authors/genres -->
     <h2
-      v-if="author !== 'defaultAuthor'"
+      v-if="author"
     >
       <v-icon>mdi-account-edit</v-icon>
       {{ author }}
     </h2>
     <h3
-      v-if="genre !== 'defaultGenre'"
+      v-if="genre"
     >
       <v-icon>mdi-filter</v-icon>
       {{ genre }}
     </h3>
     <br>
     <p
-      v-if="description !== 'defaultDescription'"
+      v-if="description"
       class="font-italic"
     >
       <v-icon>mdi-card-text-outline</v-icon>
@@ -34,10 +34,10 @@
 export default {
   name: 'ViewBookDetails',
   props: {
-    title: {type: String, default: 'defaultTitle'},
-    author: {type: String, default: 'defaultAuthor'},
-    genre: {type: String, default: 'defaultGenre'},
-    description: {type: String, default: 'defaultDescription'}
+    title: {type: String},
+    author: {type: String},
+    genre: {type: String},
+    description: {type: String}
   }
 }
 </script>
