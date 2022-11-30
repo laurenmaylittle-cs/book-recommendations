bc1ef6c8d1c47d0a7821c05a5200660a7f82e729
Marvan-T <65969444+Marvan-T@users.noreply.github.com>
2022-11-30 09:17:55 +0000

    Auth0 Backend (BES-69) (#19)
    * BES-69: Auth0 backend

---
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/security/AudienceValidator.java
@@ -0,0 +1,31 @@
+package com.bestreads.bookrecommendations.security;
+
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+/**
+ * In the auth world an audience is the intended recipient of a token. For example, if you have a
+ * token that is intended for the BestReads API, then the audience is the BestReads API. This class
+ * is used to validate that the audience of a token is the BestReads API.
+ */
+class AudienceValidator implements OAuth2TokenValidator<Jwt> {
+
+  private final String audience;
+
+  AudienceValidator(String audience) {
+    this.audience = audience;
+  }
+
+  @Override
+  public OAuth2TokenValidatorResult validate(Jwt token) {
+    OAuth2Error error = new OAuth2Error("invalid token", "Invalid required audience", null);
+
+    if (token.getAudience().contains(audience)) {
+      return OAuth2TokenValidatorResult.success();
+    }
+
+    return OAuth2TokenValidatorResult.failure(error);
+  }
+}
