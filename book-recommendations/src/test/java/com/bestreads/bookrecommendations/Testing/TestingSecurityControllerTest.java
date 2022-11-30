4fed711287b972bc153b9f0a0b27488aa8c7f003
MarvanT <marvan.t98@gmail.com>
2022-11-30 09:15:49 +0000

    BES-69: testi fixi

--- book-recommendations/src/test/java/com/bestreads/bookrecommendations/Testing/TestingSecurityControllerTest.java
+++ book-recommendations/src/test/java/com/bestreads/bookrecommendations/Testing/TestingSecurityControllerTest.java
@@ -8,7 +8,6 @@ import com.bestreads.bookrecommendations.security.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,9 +18,6 @@ class TestingSecurityControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
-
-  @MockBean
-  private TestBookRepository testBookRepository;
 
   @Test
   void returnPublic_openToPublic() throws Exception {
