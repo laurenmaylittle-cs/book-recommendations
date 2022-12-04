6f180690411908b380821e0dfe954978c68d567e
MarvanT <marvan.t98@gmail.com>
2022-12-04 00:21:44 +0000

    BES-62: filter added for best sellers page

--- book-recommendations/frontend/src/pages/HomePage.vue
+++ book-recommendations/frontend/src/pages/HomePage.vue
@@ -11,8 +11,19 @@
         indeterminate
       />
     </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-select
+          v-model="selectedCategories"
+          filled
+          :items="bestSellerCategories"
+          label="Filter by category"
+          multiple
+        />
+      </v-col>
+    </v-row>
     <book-category
-      v-for="category in categories"
+      v-for="category in filteredCategories"
       :key="category.list_id"
       :best-seller-category="category.list_name"
       :books="category.books"
@@ -29,10 +40,28 @@ export default {
   components: {BookCategory: BookCategoryCarousel},
   data: () => ({
     categories: [],
-    isLoading: true
+    isLoading: true,
+    selectedCategories: [],
   }),
+  computed: {
+    bestSellerCategories() {
+      return this.categories.map(category => {
+        return {
+          text: category.list_name,
+          value: category.list_id
+        }
+      }).sort((a, b) => a.text.localeCompare(b.text));
+    },
+    filteredCategories() {
+      if (this.selectedCategories.length === 0) {
+        return this.categories;
+      }
+      return this.categories.filter(category => this.selectedCategories.includes(category.list_id));
+    }
+  },
   async mounted() {
     this.categories = await getBestSellers();
+    this.categories.sort((a, b) => a.list_name.localeCompare(b.list_name));
     this.isLoading = false;
   }
 }
