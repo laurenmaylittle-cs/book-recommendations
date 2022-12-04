72efdfa22c06e72e9be6d4bd59da1a65ee1568c3
MarvanT <marvan.t98@gmail.com>
2022-12-04 13:59:44 +0000

    BES-62: tests added for home page component

--- book-recommendations/frontend/tests/unit/pages/HomePage.spec.js
+++ book-recommendations/frontend/tests/unit/pages/HomePage.spec.js
@@ -1,41 +1,86 @@
-import {shallowMount} from '@vue/test-utils';
 import {getBestSellers} from '@/api/home-page-api-calls';
 import HomePage from "@/pages/HomePage";
+import books from "../data/Books";
+import {shallowMount} from "@vue/test-utils";
+import bestSellerFilter from "@/components/home/BestSellerFilter";
 
 jest.mock('@/api/home-page-api-calls');
 
-function createComponentWrapper({props} = {}) {
-  return shallowMount(HomePage, {
+async function createComponentWrapper({props} = {}) {
+  const wrapper = shallowMount(HomePage, {
     propsData: {
       ...props,
     }
   });
+
+  await wrapper.vm.$nextTick(); //DOM updates in the next tick
+  return wrapper;
 }
 
-it("'Gets the best sellers", async () => {
-
+describe('HomePage', () => {
   const bestSellers = [
     {
-      "list_id": 1,
-      "list_name": "Fiction Hardcover",
-      "books": [
-        {
-          "title": "The Fellowship of the Ring",
-          "imageLinks": {
-            "smallThumbnail": null,
-            "thumbnail": "https://storage.googleapis.com/some-image.jpg"
-          },
-        }
-      ]
+      list_id: 1,
+      list_name: "Hardcover Fiction",
+      books: books
+    },
+    {
+      list_id: 2,
+      list_name: "Hardcover Nonfiction",
+      books: books.slice(0, 3)
     }
   ];
 
-  getBestSellers.mockImplementation(() => Promise.resolve(bestSellers));
+  beforeEach(() => {
+    jest.clearAllMocks();
+    getBestSellers.mockImplementation(() => Promise.resolve(bestSellers));
+  });
 
-  const wrapper = await createComponentWrapper();
+  it("'Gets the best sellers", async () => {
+    const wrapper = await createComponentWrapper();
 
-  expect(wrapper.vm.isLoading).toBe(false);
-  expect(wrapper.vm.categories).toStrictEqual(bestSellers);
-})
+    expect(wrapper.vm.isLoading).toBe(false);
+    expect(wrapper.vm.categories).toStrictEqual(bestSellers);
+  });
+
+  describe("component methods", () => {
+    it("updateBestSellerList", async () => {
+      const filteredBestSellers = [2];
+      const wrapper = await createComponentWrapper();
+
+      expect(wrapper.vm.selectedCategories).toStrictEqual([]);
+      await wrapper.findComponent(bestSellerFilter).vm.$emit('change',
+        filteredBestSellers);
+      expect(wrapper.vm.selectedCategories).toStrictEqual(filteredBestSellers);
+
+    });
+  });
+
+  describe("computed properties", () => {
+    describe("filteredCategories", () => {
+      it("returns all categories when selectedCategories is empty",
+        async () => {
+          const wrapper = await createComponentWrapper();
+
+          expect(wrapper.vm.categories).toStrictEqual(bestSellers);
+        });
+
+      it("returns selected categories when selectedCategories is not empty",
+        async () => {
+          const filteredBestSellers = [2];
+          const wrapper = await createComponentWrapper();
+
+          await wrapper.findComponent(bestSellerFilter).vm.$emit('change',
+            filteredBestSellers);
+          expect(wrapper.vm.filteredCategories).toStrictEqual([{
+            list_id: 2,
+            list_name: "Hardcover Nonfiction",
+            books: books.slice(0, 3)
+          }]);
+        });
+    });
+  });
+});
+
 
 
