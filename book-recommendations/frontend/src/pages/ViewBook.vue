83115d8afd2200da354298cecc1bdf03ca1fa370
Lauren Little <lml22@kent.ac.uk>
2022-12-21 15:50:47 +0000

    reformatted view book page

--- book-recommendations/frontend/src/pages/ViewBook.vue
+++ book-recommendations/frontend/src/pages/ViewBook.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-container>
+  <v-container
+    fill-height
+    fluid
+  >
     <v-row
       v-if="isLoading"
       class="justify-center"
@@ -11,17 +14,64 @@
         indeterminate
       />
     </v-row>
-    <v-row align="center">
-      <v-col>
+    <v-row
+      v-if="!isLoading"
+      align="center"
+      class="pb-0 pt-0"
+    >
+      <v-col class="ml-10">
+        <!-- TODO BES-70 show all authors and genres depending on the data returned from the API -->
+        <h1
+          v-if="bookData.title"
+          class="mt-0"
+        >
+          {{ bookData.title }}
+        </h1>
+        <h2
+          v-if="bookData.authors"
+        >
+          {{ bookData.authors }}
+        </h2>
+        <h3
+          v-if="bookData.categories"
+        >
+          {{ bookData.categories }}
+        </h3>
+        <br>
+        <h3 v-if="bookData.averageRating">
+          Average rating
+          <v-rating
+            v-model="bookData.averageRating"
+            background-color="orange lighten-3"
+            color="orange"
+            large
+            readonly
+          />
+        </h3>
+        <h3>
+          Your rating
+          <v-rating
+            v-model="rating"
+            background-color="orange lighten-3"
+            color="orange"
+            large
+          />
+        </h3>
+      </v-col>
+      <v-col class="pb-0 mb-0 mt-2">
         <view-book-thumbnail :thumbnail="bookData?.imageLinks?.thumbnail" />
       </v-col>
-      <v-col>
-        <!-- TODO BES-70 show all authors and genres depending on the data returned from the API -->
+    </v-row>
+    <v-row
+      v-if="!isLoading"
+      align="left"
+      class="pt-0 ma-0"
+    >
+      <v-col
+        class="pl-0 mt-0"
+      >
         <view-book-details
-          :title="bookData.title"
-          :author="bookData.authors"
-          :genre="bookData.categories"
-          :description="bookData.description"
+          :original-description="bookData.description"
         />
       </v-col>
     </v-row>
@@ -43,12 +93,12 @@ export default {
     bookData: '',
     isLoading: true
   }),
-  async mounted () {
+  async mounted() {
     await this.getBookData()
     this.isLoading = false;
   },
   methods: {
-    async getBookData () {
+    async getBookData() {
       this.bookData = await getBookInfo('9780753827666');
     }
   }
