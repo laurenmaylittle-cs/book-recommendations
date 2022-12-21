bb834e5d9938404744c3214b32e785d168a2839b
Lauren Little <lml22@kent.ac.uk>
2022-12-21 14:33:57 +0000

    fixed formatting warnings router link is now profile picture rather than text

--- book-recommendations/frontend/src/components/NavBar.vue
+++ book-recommendations/frontend/src/components/NavBar.vue
@@ -19,31 +19,41 @@
     <search-bar :search-term="searchTerm" />
     <v-spacer />
 
-<!--    TODO BES-36 do proper implementation of profile link, just placeholder to demo AuthGuard page authentication-->
-    <router-link
-      v-if="$auth.isAuthenticated"
-      to="/profile"
-      class="mr-6"
-      style="color: white; text-decoration: none"
-    >
-      Profile
-    </router-link>
+    <!--    TODO BES-36 do proper implementation of profile link, just placeholder to demo AuthGuard page authentication-->
     <div v-if="!$auth.loading">
       <!-- show login when not authenticated -->
-      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <button
+        v-if="!$auth.isAuthenticated"
+        @click="login"
+      >
+        Log in
+      </button>
       <!-- show logout when authenticated -->
       <h4 v-if="$auth.isAuthenticated">
-        Hi {{$auth.user.name}}
+        Hi {{ $auth.user.name }}
       </h4>
-      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+      <button
+        v-if="$auth.isAuthenticated"
+        @click="logout"
+      >
+        Log out
+      </button>
     </div>
     <div v-if="!$auth.loading">
-      <img
+      <router-link
         v-if="$auth.isAuthenticated"
-        :src="$auth.user.picture"
-        class="ml-3 mt-1"
-        style="border-radius: 50%; max-width:50px;"
-      />
+        to="/profile"
+        class="mr-6"
+        style="color: white; text-decoration: none"
+      >
+        <img
+          v-if="$auth.isAuthenticated"
+          alt="Profile Picture"
+          :src="$auth.user.picture"
+          class="ml-3 mt-1"
+          style="border-radius: 50%; max-width:50px;"
+        >
+      </router-link>
     </div>
   </v-app-bar>
 </template>
