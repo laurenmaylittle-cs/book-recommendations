cb9ea57fdddab8e517772f02922d6c7034b55657
MarvanT <marvan.t98@gmail.com>
2023-01-14 15:02:37 +0000

    BES-28: n+1 fixed

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/bookshelf/CollectionsRepository.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/bookshelf/CollectionsRepository.java
@@ -1,11 +1,23 @@
 package com.bestreads.bookrecommendations.bookshelf;
 
+import com.bestreads.bookrecommendations.bookshelf.model.CollectionBookProjection;
 import com.bestreads.bookrecommendations.bookshelf.model.CollectionDAO;
+import com.bestreads.bookrecommendations.bookshelf.model.CollectionProjection;
+import java.util.List;
 import java.util.Set;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CollectionsRepository extends CrudRepository<CollectionDAO, Long> {
 
-  Set<CollectionDAO> findByUserId(String userId);
+  // issues just 1 query
+  Set<CollectionProjection> findByUserId(String userId);
+
+  //this issues 1 select for each of the books - not sure how to fix this N+1
+  @EntityGraph(attributePaths = {"bookDAOS"})
+  List<CollectionBookProjection> findByIdAndUserId(Long id, String userId);
+
+  //For getting the collections by book ISBN (issues 1 query and gives you the collection Id's and names of them - N+1 tested)
+  List<CollectionProjection> findByUserIdAndBookDAOS_Isbn(String userId, String isbn);
 
 }
