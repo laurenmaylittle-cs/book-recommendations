e643a04cfad630a5a0bf3f40d29ead3d81598b4b
MarvanT <marvan.t98@gmail.com>
2023-01-23 18:07:29 +0000

    test pr

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/homepage/BestSellersService.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/homepage/BestSellersService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 class BestSellersService {
 
-  private final NyTimesService nyTimesService; //test
+  private final NyTimesService nyTimesService;
   private final NyTimesHttpResponseToBook nyTimesHttpResponseToBook;
 
   @Autowired
