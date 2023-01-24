1024780889050749fce35dcc0ae65ecf09df064a
MarvanT <marvan.t98@gmail.com>
2023-01-24 15:04:52 +0000

    test fixed

--- book-recommendations/src/test/java/com/bestreads/bookrecommendations/Testing/TestingSecurityControllerTest.java
+++ book-recommendations/src/test/java/com/bestreads/bookrecommendations/Testing/TestingSecurityControllerTest.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+@ActiveProfiles("local")
 @WebMvcTest(TestingSecurityController.class)
 @Import(SecurityConfig.class)
 class TestingSecurityControllerTest {
