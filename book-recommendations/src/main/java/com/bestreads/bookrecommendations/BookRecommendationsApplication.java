f6c5b3778b06aff18d22e01710b63a6f034f83b2
MarvanT <marvan.t98@gmail.com>
2023-01-24 14:13:20 +0000

    active profiles updated

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/BookRecommendationsApplication.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/BookRecommendationsApplication.java
@@ -2,6 +2,7 @@ package com.bestreads.bookrecommendations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class BookRecommendationsApplication {
