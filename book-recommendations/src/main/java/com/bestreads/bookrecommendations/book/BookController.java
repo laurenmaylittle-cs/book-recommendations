e62895581e6f7fea76c6c234f4318fba17b45c24
rm721 <rm721@kent.ac.uk>
2023-01-27 12:24:43 +0000

    BES-54 - display error message for invalid isbn

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/book/BookController.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/book/BookController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/api")
 public class BookController {
