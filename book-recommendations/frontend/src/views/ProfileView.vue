8daf10c6a77d1c8a2fe2be16c2ad8331488aaf09
laurenmaylittle-cs <73343109+laurenmaylittle-cs@users.noreply.github.com>
2023-01-29 15:24:55 +0000

    refactored nav bar (#39)
    added logout button to profile

--- book-recommendations/frontend/src/views/ProfileView.vue
+++ book-recommendations/frontend/src/views/ProfileView.vue
@@ -9,9 +9,28 @@
       <h2>{{ $auth.user.name }}</h2>
       <p>{{ $auth.user.email }}</p>
     </div>
+    <v-btn
+      v-if="$auth.isAuthenticated"
+      @click="logout"
+    >
+      Logout
+    </v-btn>
 
     <div>
       <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'ProfileView',
+  methods: {
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+}
+</script>
