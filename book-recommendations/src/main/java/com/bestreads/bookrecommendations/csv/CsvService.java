82d1cce60f8895a41429392c2a2c2960a52da1d9
rm721 <rm721@kent.ac.uk>
2023-02-06 13:35:32 +0000

    BES-108 - updated to use a try statement

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/csv/CsvService.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/csv/CsvService.java
@@ -10,25 +10,34 @@ import java.io.BufferedReader;
 @Service
 class CsvService {
 
-    void addToCsv(String dataToAdd, String fileName) throws IOException {
-        FileWriter fw = new FileWriter(fileName, true);
-        BufferedWriter bw = new BufferedWriter(fw);
-        bw.write(dataToAdd);
-        bw.close();
+    void addToCsv(String dataToAdd, String fileName) {
+        try (
+                FileWriter fw = new FileWriter(fileName, true);
+                BufferedWriter bw = new BufferedWriter(fw)
+        ) {
+            bw.write(dataToAdd);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    boolean checkIfBookExists(String isbn) throws IOException {
-        FileReader fr = new FileReader("books.csv");
-        BufferedReader br = new BufferedReader(fr);
+    boolean checkIfBookExists(String isbn) {
         boolean exists = false;
-        String line = br.readLine();
-        while (line != null) {
-            if (line.contains(isbn)) {
-                exists = true;
-                break;
-            } else {
-                line = br.readLine();
+        try (
+                FileReader fr = new FileReader("books.csv");
+                BufferedReader br = new BufferedReader(fr)
+                ) {
+            String line = br.readLine();
+            while (line != null) {
+                if (line.contains(isbn)) {
+                    exists = true;
+                    break;
+                } else {
+                    line = br.readLine();
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return exists;
     }
