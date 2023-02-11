13ab93f8997e5b1e5ba5b9a4e732b0e211dcbec7
MarvanT <marvan.t98@gmail.com>
2023-02-11 20:18:16 +0000

    BES-98: obliterating unused methods

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/book/BookSearchService.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/book/BookSearchService.java
@@ -53,24 +53,6 @@ public class BookSearchService {
     return httpResponseToBook.extractFromHttpResponse(httpResponse);
   }
 
-  public List<Book> searchByIsbn(String searchTerm, int maxResults) {
-    return searchByIsbn(searchTerm, 0, maxResults);
-  }
-
-  public List<Book> searchByIsbn(String searchTerm, int startIndex, int maxResults) {
-    if (maxResults < 0 || maxResults > 40) {
-      throw new IllegalArgumentException("maxResults should be between 0 and 40");
-    }
-
-    HttpResponse<String> httpResponse = googleBooksService.searchVolumeByIsbn(
-        SearchTermUtils.encodeURLTerm(searchTerm),
-        startIndex,
-        maxResults
-    );
-
-    return httpResponseToBook.extractFromHttpResponse(httpResponse);
-  }
-
   /**
    * Makes a request to Google Books API to get the book data for the given ISBN. If no book is
    * found, it makes a request to search by title and author. If no book is found, it throws an
