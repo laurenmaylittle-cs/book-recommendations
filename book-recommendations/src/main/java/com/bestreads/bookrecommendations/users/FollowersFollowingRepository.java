362bfe3b32767c0e0b56d9091b7861f3e2d0f507
laurenmaylittle-cs <73343109+laurenmaylittle-cs@users.noreply.github.com>
2023-02-22 10:03:19 +0000

    Feature/followers following (#47)
    - ability to follow a user

---
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/users/FollowersFollowingRepository.java
@@ -0,0 +1,15 @@
+package com.bestreads.bookrecommendations.users;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface FollowersFollowingRepository extends CrudRepository<FollowersFollowing, Integer> {
+
+  // This returns all people that the user follows
+  List<FollowersFollowing> findAllByFollowerEmail(String follower);
+
+  // This returns all people that are followers of the user
+  List<FollowersFollowing> findAllByFollowingEmail(String followingUser);
+}
--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/users/UsersController.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/users/UsersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +14,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsersController {
 
   private final Auth0Service auth0Service;
+  private final UsersService usersService;
 
   @Autowired
-  public UsersController(Auth0Service auth0Service) {
+  public UsersController(Auth0Service auth0Service, UsersService usersService) {
     this.auth0Service = auth0Service;
+    this.usersService = usersService;
   }
 
   @GetMapping
   public List<User> searchUsers(@Param("name") String name) {
     return auth0Service.searchUsersByName(name);
+  }
+
+  @GetMapping("followers-following-details")
+  public FollowersFollowingDetails getFollowersAndFollowingDetails(
+      @Param("currentUser") String currentUser) {
+    return usersService.getAllFollowersAndFollowing(currentUser);
+  }
+
+  @PostMapping("follow")
+  public void followUser(@Param("currentUser") String currentUser,
+      @Param("userToFollow") String userToFollow) {
+    usersService.followUser(currentUser, userToFollow);
+  }
+
+  @PostMapping("unfollow")
+  public void unfollowUser(@Param("currentUser") String currentUser,
+      @Param("userToUnfollow") String userToUnfollow) {
+    usersService.unfollowUser(currentUser, userToUnfollow);
   }
 }
