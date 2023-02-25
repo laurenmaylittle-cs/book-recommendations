7cb402bc18efa899ceb0792501e8e5abc9ab7f21
MarvanT <marvan.t98@gmail.com>
2023-02-25 13:18:23 +0000

    BookDAO is promoted to public

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/book/BookDAO.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/book/BookDAO.java
@@ -1,4 +1,4 @@
-package com.bestreads.bookrecommendations.bookshelf;
+package com.bestreads.bookrecommendations.book;
 
 import java.io.Serializable;
 import java.util.Objects;
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "books")
-class BookDAO implements Serializable {
+public class BookDAO implements Serializable {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
