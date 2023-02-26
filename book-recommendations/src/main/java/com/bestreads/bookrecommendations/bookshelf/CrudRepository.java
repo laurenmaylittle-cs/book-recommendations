dd01c34720f7b51d6574478acf6c5febd19b4889
MarvanT <marvan.t98@gmail.com>
2023-02-26 16:06:33 +0000

    obliterate CrudRepository

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/bookshelf/CrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bestreads.bookrecommendations.bookshelf;
-
-import com.bestreads.bookrecommendations.book.BookDAO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface CrudRepository extends JpaRepository<BookDAO, Long> {
-
-  BookDAO findBookDAOByIsbn(String isbn);
-}
