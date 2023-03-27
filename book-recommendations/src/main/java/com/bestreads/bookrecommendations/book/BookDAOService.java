53d41bc9419b74c852f6e16b2ba657e40ef608b8
MarvanT <marvan.t98@gmail.com>
2023-03-27 19:19:54 +0100

    Fix

--- book-recommendations/src/main/java/com/bestreads/bookrecommendations/book/BookDAOService.java
+++ book-recommendations/src/main/java/com/bestreads/bookrecommendations/book/BookDAOService.java
@@ -20,18 +20,19 @@ public class BookDAOService {
   }
 
   @Transactional
-  public BookDAO addNewBook(Book book) {
-    var currentBook = bookDAORepository.findByIsbn(book.isbn());
+  public BookDAO addNewBook(Book bookToAdd) {
+    var currentBook = bookDAORepository.findByIsbn(bookToAdd.isbn());
     if (currentBook.isEmpty()) {
       var newBook = new BookDAO();
-      newBook.setIsbn(book.isbn());
+      newBook.setIsbn(bookToAdd.isbn());
       newBook.setAuthor(
-              Optional.ofNullable(book.authors()).map(a -> String.join(", ", a)).orElse("N/A"));
-      newBook.setTitle(book.title());
-      newBook.setThumbnail(book.imageLinks().thumbnail());
-      newBook.setGenre(Optional.ofNullable(book.categories()).map(c -> c.get(0)).orElse("N/A"));
-      newBook.setPublisher(Optional.ofNullable(book.publisher()).orElse("N/A"));
-      newBook.setPublishedDate(Optional.ofNullable(book.publishedDate()).orElse("N/A"));
+          Optional.ofNullable(bookToAdd.authors()).map(a -> String.join(", ", a)).orElse("N/A"));
+      newBook.setTitle(bookToAdd.title());
+      newBook.setThumbnail(bookToAdd.imageLinks().thumbnail());
+      newBook.setGenre(
+          Optional.ofNullable(bookToAdd.categories()).map(c -> c.get(0)).orElse("N/A"));
+      newBook.setPublisher(Optional.ofNullable(bookToAdd.publisher()).orElse("N/A"));
+      newBook.setPublishedDate(Optional.ofNullable(bookToAdd.publishedDate()).orElse("N/A"));
       return bookDAORepository.save(newBook);
     } else {
       return currentBook.get();
